04aaa26d06ab3292750cf69e176d9aec67791041
HaloGamer33 <106188903+HaloGamer33@users.noreply.github.com>
2022-07-18 00:04:40 -0500

    Update Youtube-Dlp-Gui.ps1
    Added a bunch of comments and binders to make the code easier to read, may be overkill for people who are exprienced but idk i like it.

--- Youtube-Dlp-Gui.ps1
+++ Youtube-Dlp-Gui.ps1
@@ -1,17 +1,38 @@
-$invalidFormat = 0
-$moreOptions = 0
-$passthrough = 0
-$worstFormatMenu = 0
+# ---------------------------------------------------------------------------- #
+#                                Youtube Dlp Gui                               #
+# ---------------------------------------------------------------------------- #
+
+# ------------------------------- Loop Variable ------------------------------ #
+
+# This variable is run outside the loop so that the code can start.
+
 $loop = 1
 
+# -------------------------------- Main Script ------------------------------- #
+
+# Starting the loop so that the program can run until the user defines in doesnt wan to run it anymore in which case the variable $loop will be switched off ($loop = 0).
+
 while ($loop -eq 1) {
+
+    # --------------------------------- variables -------------------------------- #
+
+    # Setting some variables so they are in their default state.
+
+    $invalidFormat = 0
+    $moreOptions = 0
+    $passthrough = 0
+    $worstFormatMenu = 0
+
+    # ------------------------------- Starting Menu ------------------------------ #
 
     Write-Output ""
     Write-Output "-------------------------------------------------- YouTube Dowloader --------------------------------------------------"
     Write-Output ""
 
+    # The program ask for the link of the youtube video to the user.
 
     $link = Read-Host "Youtube Link"
+
     Write-Output ""
     Write-Output "1. for Video + Audio."
     Write-Output "2. for only Video."
@@ -21,6 +42,7 @@ while ($loop -eq 1) {
 
     $userInput = Read-Host "Choose Option"
 
+    # The program uses a switch function to compare the user input, if the user input is invalid the program uses the default yt-dlp format and proceeds with the dowload.
 
     switch ($userInput) {
         1 { $format = "$null" }
@@ -30,32 +52,40 @@ while ($loop -eq 1) {
         Default { $invalidFormat = 1 }
     }
 
+    # ---------------------------- Extra Options Menu ---------------------------- #
+
+    # If the user selected "4. for extra options" then the program displays the following:
 
     if ($moreOptions -eq 1) {
+
         Write-Output ""
         Write-Output "------------------------------------------------- Extra Options Menu --------------------------------------------------"
         Write-Output ""
         Write-Output "1. for worst formats."
         Write-Output "2. for list all formats."
         Write-Output "3. for clear log."
+        Write-Output "4. for direct youtube-dlp format."
         Write-Output ""
 
         $userInput = Read-Host "Choose Option"
-
 
         switch ($userInput) {
             1 { $worstFormatMenu = 1 }
             2 { $waitForUserFormat = 1 }
             3 { Clear-Host ; $passthrough = 1 }
+            4 { $directFormatMenu = 1 }
             Default { $invalidFormat = 1 }
         }
 
-        
+        # ---------------------------- Worst Formats Menu ---------------------------- #
+
+        # If the user selected "1. for worst formats" the program displays the following;
+
         if ($worstFormatMenu -eq 1) {
             Write-Output ""
-            Write-Output "1. Worst Video + Audio"
-            Write-Output "2. Worst Video"
-            Write-Output "3. Worst Audio"
+            Write-Output "1. Worst Video + Audio."
+            Write-Output "2. Worst Video."
+            Write-Output "3. Worst Audio."
             Write-Output ""
 
             $userInput = Read-Host "Choose Option"
@@ -68,25 +98,57 @@ while ($loop -eq 1) {
             }
         }
 
+        # --------------------------- List All Formats Menu -------------------------- #
+
+        # If the user selected "2. for list all formats." the program displays the following:
 
         switch ($waitForUserFormat) {
-            1 {         
-            Write-Output ""
-            Write-Output "--------------------------------------------------- Custom Formats ----------------------------------------------------"
-            Write-Output ""
+            1 { 
+                Write-Output ""
+                Write-Output "--------------------------------------------------- Custom Formats ----------------------------------------------------"
+                Write-Output ""
+    
+                & .\yt-dlp.exe $link "--list-formats"
+    
+                Write-Output ""
+                $customFormat = Read-Host "Choose your format number"
+                $format = "-f $customFormat"
 
-            & .\yt-dlp.exe $link "--list-formats"
-
-            Write-Output ""
-            $customFormat = Read-Host "Choose your format number"
-            $format = "-f $customFormat"
             }
-            Default { $invalidFormat = 1 }
+            Default {$invalidFormat = 1}
         }
 
+        # ---------------------------- Direct Format Menu ---------------------------- #
+
+        # If the user selected "4. for direct youtube-dlp format." the program displays the following:
+        
+        switch ($directFormatMenu) {
+            1 { 
+                Write-Output ""
+                Write-Output "------------------------------------------------ Direct Yt-Dlp Format -------------------------------------------------"
+                Write-Output ""
+
+                Write-Output "This section is for manual input of yt-dlp fomating. `n `nYou can find the information realeted to this formats in here: https://github.com/yt-dlp/yt-dlp#format-selection"
+                Write-Output ""
+
+                $format = Read-Host "Insert the format you wish to use (include -f)"
+
+            }
+            Default {$invalidFormat = 1}
+        }
     }
 
+    # ---------------------------------------------------------------------------- #
+    #                                  Yt-Dlp.exe                                  #
+    # ---------------------------------------------------------------------------- #
+
+    # The $passthrough variable is there in order to make the "Clear log command function the right way" achived through a simple if check.
+
     if ($passthrough -ne 1) {
+
+        # ------------------------- Yt-dlp.exe Initialization ------------------------ #
+
+        # Yt-Dlp starts running with the user selected options.
 
         Write-Output ""
         Write-Output "------------------------------------------------- Running YouTube-Dlp -------------------------------------------------"
@@ -95,6 +157,9 @@ while ($loop -eq 1) {
 
         & .\yt-dlp.exe $link $format
 
+        # ------------------------------ Errors Section ------------------------------ #
+
+        # If during any point of selecting options the user inputed an invalid option the porgram runs the default dowload, this let the user know what happend.
 
         if ($invalidFormat -eq 1) {
 
@@ -105,12 +170,20 @@ while ($loop -eq 1) {
 
         }
 
+        # ------------------------------ End of Program ------------------------------ #
+
+        # The program ends, and lets the user know.
+
         Write-Output ""
         Write-Output "--------------------------------------------------- End of Porgram ----------------------------------------------------"
         Write-Output ""
 
     }
 
+    # ------------------------------- Program Loop ------------------------------- #
+
+    # If the user wants to re-run the program this line ask hims, if yes the $loop variable remains unchanged to simplify process if not, then the programs displays a goodbye message.
+    
     $loopProgramAsk = Read-Host "Repeat program? y/n"
 
     switch ($loopProgramAsk) {
@@ -120,6 +193,12 @@ while ($loop -eq 1) {
     }
 
 }
+
+# ---------------------------------------------------------------------------- #
+#                                Goodbye Message                               #
+# ---------------------------------------------------------------------------- #
+
+# A goodbye message with some ascii art.
 
 Write-Output ""
 Write-Output "
@@ -157,5 +236,8 @@ Write-Output "
 
  Script by: HaloGamer 33 | Thanks for using.
 "
+
+# Thanks for using and thank you for checking my code out uwu. 
+
 Write-Output ""
 Read-Host -Prompt "Press Enter to exit"
